67bd18eb5b4924658b0f6329fc8a394d148142d0
AnshBudhiraja7 <Anshbudhiraja72@gmail.com>
2025-02-17 16:55:03 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,30 @@
+const mongoose=require("mongoose")
 const express=require("express")
 const cors=require("cors")
+const multer=require("multer")
+const fs=require("fs")
 const Connection=require("./Connection")
 const User=require("./Model/UserModel")
+const Menu=require("./Model/MenuModel")
 const app=express()
 app.use(cors())
 app.use(express.json())
+app.use('/uploads', express.static('uploads'));
 const PORT=3010
 Connection()
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        fs.mkdir("./uploads/",{recursive:true},(err)=>{
+            if(err) return cb(err,null)
+            else cb(null, 'uploads/');
+        })
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({storage});
 
 app.post("/createUser",async(req,resp)=>{
     const {name,phone,email,password}= req.body  // destructure of object
@@ -48,6 +66,58 @@ app.post("/login",async(req,resp)=>{
         resp.status(404).send({message:"Field is empty"})
     }
 })
+
+
+app.post("/createMenu",upload.single("image"),async(req,resp)=>{
+    const {item,price,category,description}=req.body
+    
+    if(item && price && category && description){
+        if(req.file.path){
+            const newMenu=await Menu.create({item,price,category,description,image:"./uploads/"+req.file.filename})
+            resp.status(201).send({message:"Menu is uploaded successfully",newMenu})
+        }
+        else{
+            resp.status(404).send({message:"Image is not uploaded"})
+        } 
+    }
+    else{
+        resp.status(404).send({message:"Field is Empty"})
+    }
+})
+app.get("/getAllMenus",async(req,resp)=>{
+    const result=await Menu.find({})
+    if(result && result.length!=0){
+        resp.status(202).send({message:"Menus fetched successfully",result})
+    }
+    else{
+        resp.status(404).send({message:"Menus not exists"})
+    }
+})
+app.delete("/deleteMenu/:id",async(req,resp)=>{
+    const {id}=req.params
+    
+    if(mongoose.isValidObjectId(id)){
+       const existingMenu=await Menu.findOne({_id:id})
+       if(existingMenu){
+        fs.unlink(existingMenu.image,(error)=>{
+            if (error){
+                resp.status(500).send({message:"Error Occured in deleting the image",error})
+            }
+        })
+        const result=await Menu.deleteOne({_id:id})
+        resp.status(202).send({message:"Menu deleted succesfully",result})
+       }
+       else{
+        resp.status(404).send({message:"Menu of this id is not found"})
+       }
+    }
+    else{
+        resp.status(401).send({message:"Object id is invalid"})
+    }  
+})
+
+
+
 
 app.listen(PORT,()=>{
     console.log("Server Started At:"+PORT);  
