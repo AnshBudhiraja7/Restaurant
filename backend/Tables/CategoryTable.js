5d4c85a6219af87c79904b82585a2fd2b5acf007
AnshBudhiraja7 <Anshbudhiraja72@gmail.com>
2025-02-27 17:03:42 +0530

    Middlewares and Category Table
    Today, we have successfully done the work of category table using queries and also created 2 API's for to create category and another one is to get all the categories from the table using token verified by the middleware function.

---
+++ backend/Tables/CategoryTable.js
@@ -0,0 +1,17 @@
+const connection = require('../Connection')();
+require("dotenv").config()
+const createCategoryTableQuery = `CREATE TABLE IF NOT EXISTS ${process.env.CATEGORY_TABLE} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        user_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES ${process.env.USER_TABLE}(id) ON DELETE CASCADE
+    );`
+
+connection.query(createCategoryTableQuery, (err) => {
+    if (err) {
+        console.error(`Error creating ${process.env.CATEGORY_TABLE} table: ${err}`);
+    } else {
+        console.log(`Table ${process.env.CATEGORY_TABLE} is ready`);
+    }
+});
+module.exports = connection;
